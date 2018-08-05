b548cda60cf40a127703df44c348c5de8e8664c0
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 14:21:07 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180805140900) do
+ActiveRecord::Schema.define(version: 20180805142023) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "password_digest"
   end
 
 end
