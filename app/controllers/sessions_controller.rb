ad6733abd1ae724f66306d9e4b5c3cf3d7843687
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 14:23:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,21 +4,14 @@ class SessionsController < ApplicationController
 
   def create
 
-    if params[:user] # Conventional Login
-        @user = User.find_by(username: params[:user][:username])
+    if params[:user]
+        @user = User.find_by(name: params[:user][:name])
         if @user && @user.authenticate(params[:user][:password])
           session[:user_id] = @user.id
           redirect_to user_path(@user)
         else
-    redirect_to (controller: 'sessions', action: 'new')
-  end
-
-
-    if !params[:name] || params[:name].empty?
-      redirect_to controller: 'sessions', action: 'new'
-    else
-      session[:name] = params[:name]
-      redirect_to controller: 'application', action: 'hello'
+          redirect_to(controller: 'sessions', action: 'new')
+        end
     end
   end
 
